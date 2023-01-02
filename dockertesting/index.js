2e5457ea90df2fafeeded1ec997967e62e2ccbaa
AbhiShetty1756 <abhishetty1756@gmail.com>
2023-01-02 07:30:37 +0530

    initial commit

---
+++ dockertesting/index.js
@@ -0,0 +1,15 @@
+var express=require('express');
+var app=express();
+var path=require('path');
+
+app.get('/',function(req,res){
+    res.sendFile(path.join(__dirname,"/public/index.html"));
+})
+
+app.get('/about',function(req,res){
+    res.send("<h1>about</h1>")
+})
+
+app.listen(3000,function(){
+    console.log("listening at port 3000")
+})
